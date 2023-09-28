31f2609b1595a70417f4349c85d0ace0d063cec5
Najlae86 <najlae.aissaoui86@gmail.com>
2023-09-28 06:37:39 -0700

    100-puppet_ssh_config.pp

---
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+# Using puppet to make changes to our configuration file
+
+file { 'ect/ssh/ssh_config':
+	ensure => present,
+content =>"
+
+	#SSH client configuration
+	host*
+	IdentityFile ~/.ssh/school
+	PasswordAuthentication no
+}
