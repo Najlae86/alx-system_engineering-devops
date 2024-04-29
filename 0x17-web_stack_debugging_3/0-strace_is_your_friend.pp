ebb1a0f8c11d0c9082a384c1bac6501c058d97af
Najlae <125600798+Najlae86@users.noreply.github.com>
2024-04-29 13:09:23 +0100

    Delete 0x17-web_stack_debugging_3 directory

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++
@@ -1,6 +0,0 @@
-#automated puppet fix (to find out why apache is returning a 500 error)
-
-exec { 'fix-wordpress':
-  command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
-  path    => '/usr/local/bin/:/bin/'
-}
